452d78f609525239f993288386d021af89ff7a26
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-03-04 12:19:08 +0200

    Add initial cmake files, formatting configs, main etc.
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2024 Renesas Electronics Corporation.
+ * Copyright (C) 2024 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int main(void)
+{
+    return 0;
+}
