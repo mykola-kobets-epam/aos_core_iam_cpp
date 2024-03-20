4b2979d8c51aff2beb1cfb5febf0da1263015b22
Mykola Kobets <mykola_kobets@epam.com>
2024-03-20 11:53:01 +0200

    [iam] Implement GRPC server
    Signed-off-by: Mykola Kobets <mykola_kobets@epam.com>

---
+++ src/iam/grpchelper.hpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2024 Renesas Electronics Corporation.
+ * Copyright (C) 2024 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef GRPCHELPER_HPP_
+#define GRPCHELPER_HPP_
+
+#include <grpcpp/security/server_credentials.h>
+
+#include <aos/common/crypto.hpp>
+#include <aos/common/cryptoutils.hpp>
+#include <aos/iam/certhandler.hpp>
+
+std::shared_ptr<grpc::ServerCredentials> GetMTLSCredentials(const aos::iam::certhandler::CertInfo& certInfo,
+    aos::cryptoutils::CertLoader& certLoader, aos::crypto::x509::ProviderItf& cryptoProvider);
+
+std::shared_ptr<grpc::ServerCredentials> GetTLSCredentials(const aos::iam::certhandler::CertInfo& certInfo,
+    aos::cryptoutils::CertLoader& certLoader, aos::crypto::x509::ProviderItf& cryptoProvider);
+
+#endif
