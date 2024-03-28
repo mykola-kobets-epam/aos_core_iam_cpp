e36d114ffd0b72f495e0fafb92327f1658dccf73
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-03-28 20:16:35 +0200

    [cmake] Fix Aos core lib exporting
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- CMakeModules/AosCoreLib.cmake
+++ CMakeModules/AosCoreLib.cmake
@@ -24,26 +24,25 @@ ExternalProject_Add(
 file(MAKE_DIRECTORY ${aoscore_build_dir}/include)
 
 add_library(aoscommon STATIC IMPORTED GLOBAL)
-add_dependencies(aoscommon aoscore)
 set_target_properties(aoscommon PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${aoscore_build_dir}/include)
 set_target_properties(aoscommon PROPERTIES IMPORTED_LOCATION ${aoscore_build_dir}/lib/libaoscommoncpp.a)
 
 add_library(aosiam STATIC IMPORTED GLOBAL)
 set_target_properties(aosiam PROPERTIES IMPORTED_LOCATION ${aoscore_build_dir}/lib/libaosiamcpp.a)
 
+add_library(mbedtls::crypto STATIC IMPORTED GLOBAL)
+set_target_properties(mbedtls::crypto PROPERTIES IMPORTED_LOCATION ${aoscore_build_dir}/lib/libmbedcrypto.a)
+
+add_library(mbedtls::mbedtls STATIC IMPORTED GLOBAL)
+set_target_properties(mbedtls::mbedtls PROPERTIES IMPORTED_LOCATION ${aoscore_build_dir}/lib/libmbedtls.a)
+
+add_library(mbedtls::mbedx509 STATIC IMPORTED GLOBAL)
+set_target_properties(mbedtls::mbedx509 PROPERTIES IMPORTED_LOCATION ${aoscore_build_dir}/lib/libmbedx509.a)
+
+add_library(mbedtls INTERFACE IMPORTED)
+set_property(TARGET mbedtls PROPERTY INTERFACE_LINK_LIBRARIES mbedtls::crypto mbedtls::mbedtls mbedtls::mbedx509)
+
 if(WITH_TEST)
-    add_library(mbedtls::crypto STATIC IMPORTED GLOBAL)
-    set_target_properties(mbedtls::crypto PROPERTIES IMPORTED_LOCATION ${aoscore_build_dir}/lib/libmbedcrypto.a)
-
-    add_library(mbedtls::mbedtls STATIC IMPORTED GLOBAL)
-    set_target_properties(mbedtls::mbedtls PROPERTIES IMPORTED_LOCATION ${aoscore_build_dir}/lib/libmbedtls.a)
-
-    add_library(mbedtls::mbedx509 STATIC IMPORTED GLOBAL)
-    set_target_properties(mbedtls::mbedx509 PROPERTIES IMPORTED_LOCATION ${aoscore_build_dir}/lib/libmbedx509.a)
-
-    add_library(mbedtls INTERFACE IMPORTED)
-    set_property(TARGET mbedtls PROPERTY INTERFACE_LINK_LIBRARIES mbedtls::crypto mbedtls::mbedtls mbedtls::mbedx509)
-
     add_library(aoscoretestutils STATIC IMPORTED GLOBAL)
     set_target_properties(aoscoretestutils PROPERTIES IMPORTED_LOCATION ${aoscore_build_dir}/lib/libtestutils.a)
 endif()
