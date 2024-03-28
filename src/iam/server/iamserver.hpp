b625d80ff8c7cbd88fdc211d35969626ebc371db
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-03-28 10:59:57 +0200

    [iamserver] Return aos::Error in Init function
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/iam/server/iamserver.hpp
+++ src/iam/server/iamserver.hpp
@@ -51,15 +51,15 @@ public:
      * @param cryptoProvider crypto provider.
      * @param provisioningMode flag indicating whether provisioning mode is active.
      */
-    void Init(const Config& config, aos::iam::certhandler::CertHandlerItf& certHandler,
+    aos::Error Init(const Config& config, aos::iam::certhandler::CertHandlerItf& certHandler,
         aos::iam::identhandler::IdentHandlerItf* identHandler, aos::iam::permhandler::PermHandlerItf* permHandler,
         RemoteIAMHandlerItf* remoteHandler, aos::cryptoutils::CertLoader& certLoader,
         aos::crypto::x509::ProviderItf& cryptoProvider, bool provisioningMode);
 
     /**
-     * Close IAM server instance.
+     * Destroys IAM server.
      */
-    void Close();
+    virtual ~IAMServer();
 
 private:
     // IAMPublicService interface
@@ -122,10 +122,10 @@ private:
         const std::string& addr, const std::shared_ptr<grpc::ServerCredentials>& credentials, bool provisionMode);
     void RegisterProtectedServices(grpc::ServerBuilder& builder, bool provisionMode);
 
-    aos::iam::certhandler::CertHandlerItf*   mCertHandler;
-    aos::iam::identhandler::IdentHandlerItf* mIdentHandler;
-    aos::iam::permhandler::PermHandlerItf*   mPermHandler;
-    RemoteIAMHandlerItf*                     mRemoteHandler;
+    aos::iam::certhandler::CertHandlerItf*   mCertHandler   = nullptr;
+    aos::iam::identhandler::IdentHandlerItf* mIdentHandler  = nullptr;
+    aos::iam::permhandler::PermHandlerItf*   mPermHandler   = nullptr;
+    RemoteIAMHandlerItf*                     mRemoteHandler = nullptr;
 
     std::string              mNodeID;
     std::string              mNodeType;
