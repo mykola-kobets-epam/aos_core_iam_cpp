274ad5ec6ab183c63ad56231fe34a1748765b451
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-06-21 13:50:20 +0300

    [cmake] Move import aoscore libs to separate AosCoreLibInclude.cmake
    It allows to include these imports for external projects.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- cmake/AosCoreLib.cmake
+++ cmake/AosCoreLib.cmake
@@ -23,32 +23,14 @@ ExternalProject_Add(
 
 file(MAKE_DIRECTORY ${aoscore_build_dir}/include)
 
-add_library(aoscommon STATIC IMPORTED GLOBAL)
-target_include_directories(aoscommon SYSTEM INTERFACE ${aoscore_build_dir}/include)
-set_target_properties(aoscommon PROPERTIES IMPORTED_LOCATION ${aoscore_build_dir}/lib/libaoscommoncpp.a)
+include(AosCoreLibInclude)
+
 add_dependencies(aoscommon aoscore)
-
-add_library(aosiam STATIC IMPORTED GLOBAL)
-set_target_properties(aosiam PROPERTIES IMPORTED_LOCATION ${aoscore_build_dir}/lib/libaosiamcpp.a)
 add_dependencies(aosiam aoscore)
-
-add_library(mbedtls::crypto STATIC IMPORTED GLOBAL)
-set_target_properties(mbedtls::crypto PROPERTIES IMPORTED_LOCATION ${aoscore_build_dir}/lib/libmbedcrypto.a)
 add_dependencies(mbedtls::crypto aoscore)
-
-add_library(mbedtls::mbedtls STATIC IMPORTED GLOBAL)
-set_target_properties(mbedtls::mbedtls PROPERTIES IMPORTED_LOCATION ${aoscore_build_dir}/lib/libmbedtls.a)
 add_dependencies(mbedtls::mbedtls aoscore)
-
-add_library(mbedtls::mbedx509 STATIC IMPORTED GLOBAL)
-set_target_properties(mbedtls::mbedx509 PROPERTIES IMPORTED_LOCATION ${aoscore_build_dir}/lib/libmbedx509.a)
 add_dependencies(mbedtls::mbedx509 aoscore)
 
-add_library(mbedtls INTERFACE IMPORTED)
-set_property(TARGET mbedtls PROPERTY INTERFACE_LINK_LIBRARIES mbedtls::mbedx509 mbedtls::mbedtls mbedtls::crypto)
-
 if(WITH_TEST)
-    add_library(aoscoretestutils STATIC IMPORTED GLOBAL)
-    set_target_properties(aoscoretestutils PROPERTIES IMPORTED_LOCATION ${aoscore_build_dir}/lib/libtestutils.a)
     add_dependencies(aoscoretestutils aoscore)
 endif()
