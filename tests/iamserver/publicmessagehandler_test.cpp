38c22344e7f810acb09093a9e748c64e019ddf09
Mykola Kobets <mykola_kobets@epam.com>
2024-08-05 03:22:29 +0300

    [iamserver] Send NodeStatus after deprovision
    Signed-off-by: Mykola Kobets <mykola_kobets@epam.com>

--- tests/iamserver/publicmessagehandler_test.cpp
+++ tests/iamserver/publicmessagehandler_test.cpp
@@ -539,24 +539,3 @@ TEST_F(PublicMessageHandlerTest, SubscribeNodeChanged)
 
     LOG_DBG() << "SubscribeNodeChanged test finished";
 }
-
-TEST_F(PublicMessageHandlerTest, RegisterNodeFailsOnPublicServerWithProvisionedNodeStatus)
-{
-    auto clientStub = CreateClientStub<iamproto::IAMPublicNodesService>();
-    ASSERT_NE(clientStub, nullptr) << "Failed to create client stub";
-
-    grpc::ClientContext context;
-
-    auto stream = clientStub->RegisterNode(&context);
-    ASSERT_NE(stream, nullptr) << "Failed to create client stream";
-
-    iamproto::IAMOutgoingMessages outgoing;
-    iamproto::IAMIncomingMessages incoming;
-
-    outgoing.mutable_node_info()->set_node_id("node0");
-    outgoing.mutable_node_info()->set_status(aos::NodeStatus(aos::NodeStatusEnum::eProvisioned).ToString().CStr());
-
-    ASSERT_TRUE(stream->Write(outgoing));
-
-    ASSERT_FALSE(stream->Read(&incoming));
-}
