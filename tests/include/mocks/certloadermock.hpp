23f610810914be73f7991dfab7cff5d239b84692
Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
2024-08-19 13:14:29 +0300

    [tests,mocks] Update cert loader mock
    Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

--- tests/include/mocks/certloadermock.hpp
+++ tests/include/mocks/certloadermock.hpp
@@ -17,7 +17,6 @@
 
 class CertLoaderItfMock : public aos::cryptoutils::CertLoaderItf {
 public:
-    MOCK_METHOD(aos::Error, Init, (aos::crypto::x509::ProviderItf&, aos::pkcs11::PKCS11Manager&), (override));
     MOCK_METHOD(aos::RetWithError<aos::SharedPtr<aos::crypto::x509::CertificateChain>>, LoadCertsChainByURL,
         (const aos::String&), (override));
     MOCK_METHOD(aos::RetWithError<aos::SharedPtr<aos::crypto::PrivateKeyItf>>, LoadPrivKeyByURL, (const aos::String&),
